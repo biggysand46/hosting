73ff33ce89988b7d1f0bb8327f5184bc5040d677
biggysand46 <130106790+biggysand46@users.noreply.github.com>
2025-04-20 12:44:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,20 @@
-const container = document.getElementById("container");
-const circle_numbers = 200;
-const max_distance = 100;
+let mousedown = false
+let svg = document.getElementById("holder");
+let cpath = null
 
-function random_number(max) {
-    return Math.floor(Math.random() * max);
-}
+document.addEventListener("mousedown", (event) => {
+    mousedown = true;
+    cpath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    cpath.setAttribute("d", `M ${event.x} ${event.y}`);
+    cpath.setAttribute("stroke", "black");
+    cpath.setAttribute("stroke-width", "3");
+    svg.appendChild(cpath);
+});
+document.addEventListener("mouseup", () => {mousedown = false});
 
-for (let i = 0; i < circle_numbers; i++) {
-    const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-
-    random_x = random_number(document.body.offsetWidth);
-    random_y = random_number(document.body.offsetHeight);
-
-    circle.setAttribute('r', '2');
-    circle.setAttribute('cx', random_x.toString());
-    circle.setAttribute('cy', random_y.toString());
-    circle.setAttribute('stroke-width', '3');
-    circle.setAttribute('fill', 'white');
-    circle.setAttribute('class', 'circle');
-
-    container.appendChild(circle);
-}
-
-var circles = document.getElementsByClassName("circle");
-
-for (let i = 0; i < circle_numbers; i++) {
-    cx = parseInt(container.children[i].getAttribute("cx"));
-    cy = parseInt(container.children[i].getAttribute("cy"));
-
-    line = document.createElementNS('http://www.w3.org/2000/svg','line');
-
-    line.setAttribute('x1', cx);
-    line.setAttribute('y1', cy);
-    line.setAttribute('x2', cx);
-    line.setAttribute('y2', cy);
-    line.setAttribute("stroke", "white");
-    line.setAttribute("class", "liner");
-
-    container.append(line);
-}
-
-document.addEventListener("mousemove", function(event) {
-    mx = event.clientX;
-    my = event.clientY;
-    for (let i = 0; i < container.children.length; i++) {
-        if (container.children[i].classList == "liner") {
-            line = container.children[i];
-
-            x1 = parseInt(line.getAttribute("x1"));
-            y1 = parseInt(line.getAttribute("y1"));
-
-            distance = Math.sqrt(((x1-mx)**2) + ((y1-my)**2));
-
-            if (distance <= max_distance) {
-                line.setAttribute('x2', event.clientX);
-                line.setAttribute('y2', event.clientY);
-            } else {
-                line.setAttribute('x2', x1);
-                line.setAttribute('y2', y1);
-            }
-        }
-    } 
+document.addEventListener("mousemove", (event) => {
+    if (mousedown) {
+        let currentpath = cpath.getAttribute("d");
+        cpath.setAttribute("d", `${currentpath} l ${event.x} ${event.y}`);
+    }
 });
